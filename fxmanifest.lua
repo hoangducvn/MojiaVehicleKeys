6ddf1a5735865f8888b5e468fb237585099e82d2
Carlos Araújo <carlosaaraujopc@gmail.com>
2022-02-02 12:13:19 -0500

    Update fxmanifest.lua
    Comma that was missing in fxmanifest.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,12 +7,12 @@ description 'MojiaVehicleKeys - Best vehicle keys for MojiaCity Framework'
 
 shared_scripts {
 	'@qb-core/shared/locale.lua',
-    'locales/en.lua' -- Change this to your preferred language
+    	'locales/en.lua', -- Change this to your preferred language
 	'config.lua'
 }
 client_script 'client.lua'
 server_scripts {
-	'@oxmysql/lib/MySQL.lua',
+    '@oxmysql/lib/MySQL.lua',
     'server.lua'
 }
 
